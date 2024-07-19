3a23b69c394ca9d3c29ad664271c9cfc233fcb4c
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-07-19 20:05:25 +0400

    static created

--- src/components/HomePageList.tsx
+++ src/components/HomePageList.tsx
@@ -4,16 +4,30 @@ import arrowRight from "/assets/shared/desktop/icon-arrow-right.svg";
 
 export default function HomePageList() {
   return (
-    <div className="parent flex flex-col gap-[68px] items-center">
+    <div className="parent flex flex-col  items-center px-[24px] gap-[60px] mb-[120px] mt-[70px] ">
       {
         <>
           {data[data.length - 1].info?.map((item) => {
             return (
-              <div>
-                <img src={item.source} alt="" />
-                <h1>{item.name}</h1>
-                <span>{item.text}</span>
-                <img src={arrowRight} alt="" />
+              <div className="flex flex-col items-center w-full bg-[#F1F1F1] rounded-[8px] pb-[15px] ">
+                <img
+                  src={item.source}
+                  alt=""
+                  style={{
+                    width: item.size.width,
+                    height: item.size.height,
+                    marginTop: "-14%",
+                  }}
+                />
+                <h1 className="text-[#000] text-[15px] font-bold tracking-[1.071px] uppercase mb-[15px]">
+                  {item.name}
+                </h1>
+                <div className="flex gap-[13px] items-center">
+                  <span className="text-[#000] text-[13px] font-bold tracking-[1.071px] uppercase opacity-50">
+                    {item.text}
+                  </span>
+                  <img src={item.arrow} alt="" />
+                </div>
               </div>
             );
           })}
