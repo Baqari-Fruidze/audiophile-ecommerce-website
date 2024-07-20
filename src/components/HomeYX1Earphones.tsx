c449823a4407f6da93ead5c765ea0f8916de1645
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-07-20 19:38:38 +0400

    middle

---
+++ src/components/HomeYX1Earphones.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import earphonEMobile from "/assets/home/mobile/image-earphones-yx1.jpg";
+import { Link } from "react-router-dom";
+
+export default function HomeYX1Earphones() {
+  return (
+    <div className="flex flex-col items-center gap-[24px] ">
+      <img src={earphonEMobile} alt="" className="rounded-[8px]" />
+      <div className="bg-[#F1F1F1] py-[41px] pl-[24px] w-full rounded-[8px]">
+        <h1 className="text-[28px] text-[#000] font-bold tracking-[2px] uppercase mb-[32px]">
+          YX1 EARPHONES
+        </h1>
+        <Link className="text-[#000] text-[13px] font-bold tracking-[1px] uppercase py-[14px] px-[31px] border-[1px] border-solid border-[#000] ">
+          See Product
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/pages/MiddleSingleProduct.tsx
+++ src/pages/MiddleSingleProduct.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Homezx9Speaker from "../components/Homezx9Speaker";
 import Homezx7Speaker from "../components/Homezx7Speaker";
+import HomeYX1Earphones from "../components/HomeYX1Earphones";
 
 export default function MiddleSingleProduct() {
   return (
     <div
-      className={`px-[24px] flex flex-col gap-[24px] items-center mb-[50px]`}
+      className={`px-[24px] flex flex-col gap-[24px] items-center mb-[120px]`}
     >
       <Homezx9Speaker />
       <Homezx7Speaker />
+      <HomeYX1Earphones />
     </div>
   );
 }
